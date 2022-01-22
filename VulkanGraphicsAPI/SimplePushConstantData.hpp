016b9cc95d79a60f669924722d7bee3364bd7e90
CamSoalSHU <56632388+CameronSoal-SHU@users.noreply.github.com>
2022-01-22 20:46:16 +0000

    Removed Temp code

--- VulkanGraphicsAPI/SimplePushConstantData.hpp
+++ VulkanGraphicsAPI/SimplePushConstantData.hpp
@@ -5,6 +5,6 @@ namespace SolEngine::Data
 	{
 		glm::mat2 transform{ 1.0f };
 		glm::vec2 offset;
-		alignas(16) glm::vec3 colour;	// TEMP: Needs to be aligned in memory as the shader expects it, as by default it's tightly packed.
+		alignas(16) glm::vec3 colour;
 	};
 }
