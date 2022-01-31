49d6cefdc701cf2d0563408160c5c2b999668ff1
CAMERON SOAL <b8010556@hallam.shu.ac.uk>
2022-01-31 13:44:37 +0000

    Reduced max grid nodes to 256 for testing

--- VulkanGraphicsAPI/Constants.hpp
+++ VulkanGraphicsAPI/Constants.hpp
@@ -7,5 +7,5 @@ namespace SolEngine::Data
 	static constexpr glm::vec3 VEC3_AXIS_Y{ 0.f, -1.f, 0.f };
 	static constexpr glm::vec3 VEC3_AXIS_Z{ 0.f, 0.f, 1.f };
 
-	static constexpr size_t MAX_GRID_NODES{ 1 << 24 };
+	static constexpr size_t MAX_GRID_NODES{ 1 << 8 };
 }
