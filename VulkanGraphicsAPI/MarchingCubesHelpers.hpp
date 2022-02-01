463374fb88badb19798f78cd2c349f034c322782
CAMERON SOAL <b8010556@hallam.shu.ac.uk>
2022-02-01 17:00:16 +0000

    Renamed VerticesIndexFromEdgeIndex to CornerIndicesFromEdgeIndex to be more specific

---
+++ VulkanGraphicsAPI/MarchingCubesHelpers.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+namespace Utility
+{
+    static std::tuple<size_t, size_t> CornerIndicesFromEdgeIndex(const size_t edgeIndex)
+    {
+        switch (edgeIndex)
+        {
+        case 0:
+            return { 0, 1 };
+        case 1:
+            return { 1, 2 };
+        case 2:
+            return { 2, 3 };
+        case 3:
+            return { 3, 0 };
+        case 4:
+            return { 4, 5 };
+        case 5:
+            return { 5, 6 };
+        case 6:
+            return { 6, 7 };
+        case 7:
+            return { 7, 4 };
+        case 8:
+            return { 0, 4 };
+        case 9:
+            return { 1, 5 };
+        case 10:
+            return { 2, 6 };
+        case 11:
+            return { 3, 7 };
+        default:
+            return { -1, -1 };
+        }
+    }
+}
