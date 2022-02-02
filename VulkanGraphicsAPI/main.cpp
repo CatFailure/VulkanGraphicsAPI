0a65ce8ae8425f551afe4bd0fa707308ae372d39
CAMERON SOAL <b8010556@hallam.shu.ac.uk>
2022-02-02 01:41:03 +0000

    Changed WinTitle

--- VulkanGraphicsAPI/main.cpp
+++ VulkanGraphicsAPI/main.cpp
@@ -5,7 +5,7 @@ int main()
 {
 	const ApplicationData appData
 	{
-		.windowTitle	  = "[Development] ImGui Integration",
+		.windowTitle	  = "[Feature] Marching Cubes",
 		.windowClassName  = L"VulkanWindowClass",
 		.engineName		  = "SolEngine",
 		.appName		  = "VulkanGraphicsAPI",
