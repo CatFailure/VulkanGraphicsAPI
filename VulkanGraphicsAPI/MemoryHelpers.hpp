dbe66425e13e4ce19f088239fc1bbdb47ea210c0
CAMERON SOAL <b8010556@hallam.shu.ac.uk>
2022-02-08 00:16:27 +0000

    Increased Max cubes per axis to 1 << 8

--- VulkanGraphicsAPI/MemoryHelpers.hpp
+++ VulkanGraphicsAPI/MemoryHelpers.hpp
@@ -22,17 +22,21 @@ namespace Utility
                                                const size_t height)
     {
         // https://stackoverflow.com/a/29375830
-        const size_t sizeOfType = sizeof(_Ty);
+        const size_t sizeOfType    = sizeof(_Ty);
         const size_t sizeOfPtrType = sizeof(_Ty *);
-        const size_t alignment = 16U;
+        const size_t arraySizeBytes = width * sizeOfPtrType;
+        const size_t arrayElementsSizeBytes = width * height * sizeOfType;
+        const size_t alignment     = 16U;
 
-        pprArr    = (_Ty **)_aligned_malloc(width * sizeOfPtrType, alignment);
+        pprArr = (_Ty **)_aligned_malloc(arraySizeBytes, alignment);
 
         DBG_ASSERT_MSG((pprArr != nullptr), "_aligned_malloc Failed!");
+        printf_s("Allocated: %zu bytes.\n", arraySizeBytes);
 
-        pprArr[0] = (_Ty *)_aligned_malloc(width * height * sizeOfType, alignment);
+        pprArr[0] = (_Ty *)_aligned_malloc(arrayElementsSizeBytes, alignment);
 
         DBG_ASSERT_MSG((pprArr[0] != nullptr), "_aligned_malloc Failed!");
+        printf_s("Allocated: %zu bytes.\n", arrayElementsSizeBytes);
 
         for (size_t i(1); i < width; ++i)
         {
