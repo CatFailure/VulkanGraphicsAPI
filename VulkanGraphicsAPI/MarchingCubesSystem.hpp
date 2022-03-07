2c2889e87af287abd700b6d3b44314a5dde01280
CamSoalSHU <56632388+CameronSoal-SHU@users.noreply.github.com>
2022-03-07 15:16:08 +0000

    Formatting

--- VulkanGraphicsAPI/MarchingCubesSystem.hpp
+++ VulkanGraphicsAPI/MarchingCubesSystem.hpp
@@ -14,13 +14,13 @@ namespace SolEngine::System
 		MarchingCubesSystem(SolDevice& rSolDevice, SolGrid& rSolGrid);
 
 		const SolGameObject& GetGameObject() const { return _marchingCubesObject; }
-		size_t GetIsoValuesGeneratedCount() const { return _isoValuesGeneratedCount; }
+		size_t GetIsoValuesGeneratedCount()  const { return _isoValuesGeneratedCount; }
 
 		void March(DiagnosticData& rDiagnosticData);
 
 	private:
-		uint32_t GetCubeIndex(const bool* pNodeStates);
-		void CreateVertices(Cells& rNodes, const Index_t* pEdgeIndices, const uint32_t xIndex, const uint32_t yIndex, const uint32_t zIndex);
+		uint32_t  GetCubeIndex(const bool* pNodeStates);
+		void	  CreateVertices(Cells& rNodes, const Index_t* pEdgeIndices, const uint32_t xIndex, const uint32_t yIndex, const uint32_t zIndex);
 		glm::vec3 GetEdgeVertexPosition(Cells& rNodes, const uint32_t xIndex, const uint32_t yIndex, const uint32_t zIndex,
 										const std::pair<Index_t, Index_t>& cornerIndices);
 		void UpdateGameObjectModel();
