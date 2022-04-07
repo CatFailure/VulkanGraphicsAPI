3866ea7e8345fb1c388bec799bdfcb34869bc8f2
CAMERON SOAL <b8010556@hallam.shu.ac.uk>
2022-04-07 11:26:26 +0100

    Added MouseButton enum

--- VulkanGraphicsAPI/Cursor.hpp
+++ VulkanGraphicsAPI/Cursor.hpp
@@ -1,7 +1,14 @@
 #pragma once
+#include "MouseButton.hpp"
+
+using namespace SolEngine::Enumeration;
 
 namespace SolEngine::Input
 {
     struct Cursor
-    {};
+    {
+        bool mouseButtons[(size_t)MouseButton::COUNT]{ false };
+
+        bool isButtonDown(const MouseButton button) const { return mouseButtons[(size_t)button]; }
+    };
 }
