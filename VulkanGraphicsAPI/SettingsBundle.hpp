7182c34e380c827d702b21b366df78c81302e778
CAMERON SOAL <b8010556@hallam.shu.ac.uk>
2022-05-04 22:34:23 +0100

    Added deserialization code for SettingsBundle

--- VulkanGraphicsAPI/SettingsBundle.hpp
+++ VulkanGraphicsAPI/SettingsBundle.hpp
@@ -7,12 +7,42 @@
 
 namespace SolEngine::Settings
 {
-    struct SettingsBundle
+    struct SettingsBundle : public ISerializable
     {
+		virtual bool Deserialize(const rapidjson::Value& obj) override
+		{
+			if (!JsonHasMembers(obj, 
+								PROP_SETTINGS_CAMERA,
+								PROP_SETTINGS_GAME_OF_LIFE,
+								PROP_SETTINGS_GRID, 
+								PROP_SETTINGS_RENDER, 
+								PROP_SETTINGS_SIMULATION))
+			{
+				DBG_ASSERT_MSG(false, "[SimulationSettings] Object Missing Members!");
+
+				return false;
+			}
+
+			cameraSettings.Deserialize(obj[PROP_SETTINGS_CAMERA].GetObj());
+			gameOfLifeSettings.Deserialize(obj[PROP_SETTINGS_GAME_OF_LIFE].GetObj());
+			gridSettings.Deserialize(obj[PROP_SETTINGS_GRID].GetObj());
+			renderSettings.Deserialize(obj[PROP_SETTINGS_RENDER].GetObj());
+			simulationSettings.Deserialize(obj[PROP_SETTINGS_SIMULATION].GetObj());
+
+			return true;    // OK!
+		}
+
         CameraSettings     cameraSettings    {};
         GameOfLifeSettings gameOfLifeSettings{};
         GridSettings       gridSettings      {};
         RenderSettings     renderSettings    {};
         SimulationSettings simulationSettings{};
+
+	private:
+		static constexpr const char* PROP_SETTINGS_CAMERA	   { "camera_settings" };
+		static constexpr const char* PROP_SETTINGS_GAME_OF_LIFE{ "game_of_life_settings" };
+		static constexpr const char* PROP_SETTINGS_GRID		   { "grid_settings" };
+		static constexpr const char* PROP_SETTINGS_RENDER	   { "render_settings" };
+		static constexpr const char* PROP_SETTINGS_SIMULATION  { "simulation_settings" };
     };
 }
